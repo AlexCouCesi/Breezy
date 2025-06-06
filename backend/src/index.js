46bf91a16d5502081865f49d55fbc5f0eb790cf1
Alexandre Cougny <alexandre.cougny@viacesi.fr>
2025-06-06 15:14:41 +0200

    Initialisation projet 1

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connecté'))
+    .catch((err) => console.error(err));
+
+// Routes ici
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(5000, () => console.log('Serveur backend sur http://localhost:5000'));
