9db381b8b0b5f5cf363dbaf80fe0b6656eb6aff9
Alexandre <alexandre.cougny@viacesi.fr>
2025-06-21 14:30:30 +0200

    Rétablissement de users

--- backend/users/src/index.js
+++ backend/users/src/index.js
@@ -1,28 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import usersRouter from './routes/users.routes.js';
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('Connected to MongoDB for users');
-  app.listen(port, () => {
-    console.log(`Users service running on http://localhost:${port}`);
-  });
-})
-.catch((err) => {
-  console.error('MongoDB connection failed:', err);
-  process.exit(1);
-});
-
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
-app.use(cookieParser());
+// Middleware
 app.use(express.json());
+
+// Routes
 app.use('/api/users', usersRouter);
+
+// Mongo connection + server start
+mongoose
+    .connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE_NAME}`)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(port, () => {
+        console.log(`Users service running at http://localhost:${port}`);});
+    })
+    .catch((err) => {
+    console.error('MongoDB connection failed:', err);
+    process.exit(1);
+    });
