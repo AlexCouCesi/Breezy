69f2cac6b489e100ad772913262c5a749b9bd6e8
t-i-m-o-n <fourniertimon@gmail.com>
2025-06-24 16:21:40 +0200

    correction chemin relatif des photos de profil

--- frontend/src/components/followedlist.jsx
+++ frontend/src/components/followedlist.jsx
@@ -13,22 +13,26 @@ export default function FollowedList({ followingList }) {
         );
     } else {
         return (
-            <aside className="w-80 bg-white border-l p-6 hidden lg:block">
-                <h2 className="text-xl font-semibold mb-4">Comptes suivis</h2>
-                <ul className="space-y-4">
-                    {followingList.map((user, index) => (
-                        <li key={index} className="flex items-center gap-3">
-                            <Link href={`/profile/${user._id}`} className="flex items-center gap-3 hover:underline">
-                                <img
-                                    src={user.profilePicture || '/assets/icones_divers/profile_icon.png'}
-                                    alt={user.username}
-                                    className="w-10 h-10 rounded-full object-cover bg-gray-300"
-                                />
-                                <span className="text-gray-800">@{user.username}</span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+            <aside className="w-80 bg-white border-l p-6 hidden lg:block h-screen">
+            <h2 className="text-xl font-semibold mb-4">Comptes suivis</h2>
+            <ul className="space-y-4">
+                {followingList.map((user) => (
+                <li key={user._id} className="flex items-center gap-3">
+                    <Link href={`/profile/${user._id}`} className="flex items-center gap-3 hover:underline">
+                    <img
+                        src={
+                        user.profilePicture
+                            ? `${process.env.NEXT_PUBLIC_API_URL}/${user.profilePicture}`
+                            : `${process.env.NEXT_PUBLIC_API_URL}/assets/icones_divers/profile_icon.png`
+                        }
+                        alt={user.username}
+                        className="w-10 h-10 rounded-full object-cover bg-gray-300"
+                    />
+                    <span className="text-gray-800">@{user.username}</span>
+                    </Link>
+                </li>
+                ))}
+            </ul>
             </aside>
         );
     }
